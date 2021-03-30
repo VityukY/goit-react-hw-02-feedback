cf3c5a1e55c74a60c1ddc1c5ffca4fde92e66a88
Vityuk Yuriy <you.thunderstruck@gmail.com>
2021-03-30 23:04:43 +0300

    Updates

---
+++ static/js/main.5d700c6b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),a=c(3),s=c.n(a),i=c(0);var o=function(){return Object(i.jsx)("div",{className:"App"})};c(9),c(10);s.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},9:function(e,t,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.5d700c6b.chunk.js.map
