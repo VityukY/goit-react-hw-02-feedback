6cfb9291a33d3beb0aad4e88045ba571bda0db4b
Vityuk Yuriy <you.thunderstruck@gmail.com>
2021-04-07 22:33:47 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import styles from './components/feedback/styles.module.css';
 import 'modern-normalize/modern-normalize.css';
 
 class App extends Component {
-
    state = {
       good: 0,
       neutral: 0,
@@ -15,13 +14,12 @@ class App extends Component {
    };
 
    incrementCounter = id => {
-            this.setState(prevState => {
+      this.setState(prevState => {
          return {
-            [id]: prevState.[id] + 1,
+            [id]: prevState[id] + 1,
          };
-      })
-
-   }
+      });
+   };
 
    counterTotalFeedback = () => {
       return this.state.good + this.state.neutral + this.state.bad;
@@ -34,20 +32,30 @@ class App extends Component {
    };
 
    render() {
-      
       return (
          <div>
             <Section>
-               <h1 className={styles.title} >Please leave feedback</h1>
-               <ControlsFeedback options={this.state} incrementCounter={this.incrementCounter} />
+               <h1 className={styles.title}>Please leave feedback</h1>
+               <ControlsFeedback
+                  options={this.state}
+                  incrementCounter={this.incrementCounter}
+               />
             </Section>
             <Section>
                <h2 className={styles.title}>Statistics</h2>
-               {this.counterTotalFeedback() === 0 ? (<Notification message="No feedback given" />) : (
+               {this.counterTotalFeedback() === 0 ? (
+                  <Notification message="No feedback given" />
+               ) : (
                   <Statistics optins={this.state}>
-                  <p> Total: {this.counterTotalFeedback()}</p>
-                  <p> Positive feedback: {this.countPositiveFeedbackPercentage() === 'NaN' ? '0' : this.countPositiveFeedbackPercentage()}%</p>
-               </Statistics>
+                     <p> Total: {this.counterTotalFeedback()}</p>
+                     <p>
+                        Positive feedback:{' '}
+                        {this.countPositiveFeedbackPercentage() === 'NaN'
+                           ? '0'
+                           : this.countPositiveFeedbackPercentage()}
+                        %
+                     </p>
+                  </Statistics>
                )}
             </Section>
          </div>
